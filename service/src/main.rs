84fd545a040fffa6d06faee8ddc610ab3f25ad20
Stefan Schindler <dns2utf8@estada.ch>
2017-01-02 01:11:10 +0100

    Implement CORS

--- service/src/main.rs
+++ service/src/main.rs
@@ -35,30 +35,54 @@ fn main() {
     // try it with curl
     // curl 'http://localhost:6767/' -H 'Content-Type: application/json;charset=UTF-8'  --data-binary $'{"version":"0.13", "schema":"{...}"}'
     server.post("/", middleware! { |request, mut response|
-        response.headers_mut().set_raw("Access-Control-Allow-Origin", vec![b"*".to_vec()]);
-        response.headers_mut().set_raw("Access-Control-Allow-Methods", vec![b"POST,OPTIONS".to_vec()]);
+        set_headers(&mut response);
 
         let vr = try_with!(response, {
             request.json_as::<ValidationRequest>().map_err(|e| (StatusCode::BadRequest, e))
         });
 
-
-        // TODO: insert validation here
-
-        //format!("Hello {} {}", person.version, person.schema)
-        encode(&Result {
-            version: "0.13".into(),
+        let result = spaceapi_validator::validate_spaceapi_json(&*vr.schema);
+        let result = match result {
+          Ok(state) => Result {
+            version: "TODO".into(),
             status: "OK".into(),
             message: None,
-        }).unwrap()
+          },
+          Err(boxed_error) => {
+            let err_msg: &str = (*boxed_error).description();
+            Result {
+              version: "TODO".into(),
+              status: "ERROR".into(),
+              message: Some(err_msg.into()),
+            }
+          },
+        };
+
+        //format!("Hello {} {}", person.version, person.schema)
+        encode(&result).unwrap()
     });
 
+
+    // Supporting CORS
     server.get("/", middleware! { |request, mut response|
-        response.headers_mut().set_raw("Access-Control-Allow-Origin", vec![b"*".to_vec()]);
-        response.headers_mut().set_raw("Access-Control-Allow-Methods", vec![b"POST,OPTIONS".to_vec()]);
+        set_headers(&mut response);
+
+        "OK"
+    });
+
+    server.options("/", middleware! { |request, mut response|
+        set_headers(&mut response);
 
         "OK"
     });
 
     server.listen("127.0.0.1:6767").unwrap();
+}
+
+fn set_headers(response: &mut nickel::Response) {
+    response.headers_mut().set_raw("Access-Control-Allow-Origin", vec![b"*".to_vec()]);
+    response.headers_mut().set_raw("Access-Control-Allow-Methods", vec![b"GET, POST, OPTIONS".to_vec()]);
+    response.headers_mut().set_raw("Access-Control-Allow-Headers", vec![b"Origin, Content-Type, X-Auth-Token".to_vec()]);
+
+    response.headers_mut().set_raw("Cache-Control", vec![b"no-cache".to_vec()]);
 }
